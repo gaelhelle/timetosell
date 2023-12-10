57764e4587d0d9becdcf31cb4529c788c2073eca
Gael (perso) <me@gaelhelle.fr>
2023-12-09 23:26:47 -0800

    Update cache time for /api

--- client/app/api/route.ts
+++ client/app/api/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { getRankValues } from "@/utils/supabase";
 import { getUniqueValuesPerDay } from "@/utils/utils";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const values = await getRankValues();
   const uniqueValues = getUniqueValuesPerDay(values);
